ef5b12357920b461adafba69c0c1246a89f5de87
西村依泰 <yori3.otim2@gmail.com>
2019-12-17 22:14:59 +0900

    aboutページ　ブロックエディタを意識した構成を考え中（いつか変える）

--- page-about.php
+++ page-about.php
@@ -17,52 +17,52 @@
       </div>
 
 
-      <div class="about__block-profile">
+      <div class="about__block about__block-profile">
         <h2 class="about__ttl">Profile</h2>
         <div class="profile__img"><img src="<?php echo get_template_directory_uri(); ?>/images/img_mine.jpg" alt=""></div>
         <dl class="profile__content profileList">
-          <dt class="profileList__term">Name</dt>
-          <dd class="profileList__description">西村　依泰</dd>
-          <dt class="profileList__term">Birth/Age</dt>
-          <dd class="profileList__description">1988.04.01／30歳</dd>
-          <dt class="profileList__term">Live</dt>
-          <dd class="profileList__description">大阪府</dd>
-          <dt class="profileList__term">Work</dt>
-          <dd class="profileList__description">元小学校教諭（〜2015）<br>マークアップ・CSSコーディング（2016〜）<br>ディレクション（2018〜）</dd>
-          <dt class="profileList__term">hobby</dt>
-          <dd class="profileList__description">楽器（ピアノ・ギター）、推理小説、TVゲーム</dd>
-          <dt class="profileList__term">Like</dt>
-          <dd class="profileList__description">コーヒー、甘味</dd>
-          <dt class="profileList__term">Community</dt>
-          <dd class="profileList__description">Word Camp Kansai 2016 実行委員<br>Word Camp Kyoto 2017 実行委員<br>Word Camp Osaka 2018 実行委員</dd>
+          <dt>Name</dt>
+          <dd>西村　依泰</dd>
+          <dt>Birth/Age</dt>
+          <dd>1988.04.01／30歳</dd>
+          <dt>Live</dt>
+          <dd>大阪府</dd>
+          <dt>Work</dt>
+          <dd>元小学校教諭（〜2015）<br>マークアップ・CSSコーディング（2016〜）<br>ディレクション（2018〜）</dd>
+          <dt>hobby</dt>
+          <dd>楽器（ピアノ・ギター）、推理小説、TVゲーム</dd>
+          <dt>Like</dt>
+          <dd>コーヒー、甘味</dd>
+          <dt>Community</dt>
+          <dd>Word Camp Kansai 2016 実行委員<br>Word Camp Kyoto 2017 実行委員<br>Word Camp Osaka 2018 実行委員</dd>
         </dl>
       </div>
 
-      <div class="about__block-skill">
+      <div class="about__block about__block-skill">
         <h2 class="about__ttl">Skill</h2>
-        <div class="skill__content-tools">
-          <h3 class="skill__ttl">Tools</h3>
-          <ul class="skill__list">
-            <li class="skill__list-item">Photoshop</li>
-            <li class="skill__list-item">Illustrator</li>
-            <li class="skill__list-item">Adobe XD</li>
-            <li class="skill__list-item">Dreamweaver</li>
-            <li class="skill__list-item">Atom</li>
-            <li class="skill__list-item">Google Analytics</li>
-            <li class="skill__list-item">Github</li>
+        <div class="skill__content skill__content-tools">
+          <h3>Tools</h3>
+          <ul>
+            <li>Photoshop</li>
+            <li>Illustrator</li>
+            <li>Adobe XD</li>
+            <li>Dreamweaver</li>
+            <li>Atom</li>
+            <li>Google Analytics</li>
+            <li>Github</li>
           </ul>
         </div>
-        <div class="skill__content-language">
-          <h3 class="skill__ttl">Language</h3>
-          <ul class="skill__list">
-            <li class="skill__list-item">HTML</li>
-            <li class="skill__list-item">CSS</li>
-            <li class="skill__list-item">SCSS</li>
-            <li class="skill__list-item">Javascript</li>
-            <li class="skill__list-item">jQuery</li>
-            <li class="skill__list-item">Vue.js</li>
-            <li class="skill__list-item">PHP</li>
-            <li class="skill__list-item">WordPress</li>
+        <div class="skill__content skill__content-language">
+          <h3>Language</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SCSS</li>
+            <li>Javascript</li>
+            <li>jQuery</li>
+            <li>Vue.js</li>
+            <li>PHP</li>
+            <li>WordPress</li>
           </ul>
         </div>
       </div>
